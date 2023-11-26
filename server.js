213cde73d746e451e412c57b8c34232b18e9ee8d
Kwoa69 <96543164+Kwoa69@users.noreply.github.com>
2023-11-26 19:12:07 +0100

    add removeuserfromwhitelist command

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const bodyParser = require('body-parser');
 const auth = require('basic-auth');
 const fs = require('fs');
 const morgan = require('morgan');
+const WebSocket = require('ws');
 
 const app = express();
+const server = require('http').createServer(app);
+const wss = new WebSocket.Server({ server });
+
 const port = 3001;
 
 const configFile = fs.readFileSync('config.json');
@@ -36,24 +40,35 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/buttonClick', (req, res) => {
-    const { button, username } = req.body;
-    const logMessage = `L'utilisateur ${username} a cliqué sur le bouton : ${button}`;
-    console.log(logMessage);
-    accessLogStream.write(logMessage + '\n');
-    res.send(`Clic sur le bouton enregistré : ${button}`);
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+    });
 });
+
+function sendOutputToClients(output) {
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ output: output }));
+        }
+    });
+}
 
 app.post('/runRcon', (req, res) => {
     const rconCommand = `rcon.exe -a ${serverConfig.ip}:${serverConfig.port} -p ${serverConfig.rconPassword} "${req.body.command}"`;
 
     exec(rconCommand, (error, stdout, stderr) => {
         if (error) {
-            console.error(`Erreur lors de l'exécution de rcon.exe : ${error}`);
+            const errorMessage = `Erreur lors de l'exécution de rcon.exe : ${error}`;
+            console.error(errorMessage);
+            sendOutputToClients(errorMessage);
             return res.status(500).send('Erreur lors de l\'exécution de rcon.exe');
         }
 
         console.log(`Sortie de rcon.exe : ${stdout}`);
+        sendOutputToClients(`Sortie de rcon.exe : ${stdout}`);
 
         if (req.body.command.toLowerCase() === 'players') {
             return res.send(stdout);
@@ -64,7 +79,8 @@ app.post('/runRcon', (req, res) => {
 });
 
 app.post('/runRconBanId', (req, res) => {
-    const banIdCommand = `rcon.exe -a ${serverConfig.ip}:${serverConfig.port} -p ${serverConfig.rconPassword} banid ${req.body.banIdValue}`;
+    const banIdValue = req.body.banIdValue; // Récupérer la valeur directement
+    const banIdCommand = `rcon.exe -a ${serverConfig.ip}:${serverConfig.port} -p ${serverConfig.rconPassword} "banid ${banIdValue}"`;
 
     exec(banIdCommand, (error, stdout, stderr) => {
         if (error) {
@@ -73,24 +89,29 @@ app.post('/runRconBanId', (req, res) => {
         }
 
         console.log(`Sortie de rcon.exe (banid) : ${stdout}`);
-        res.send('Commande RCON (banid) exécutée avec succès !');
+        res.send(`Commande RCON (banid) exécutée avec succès : ${stdout}`);
     });
 });
-		app.post('/runRconAddUser', (req, res) => {
+
+
+
+app.post('/runRconAddUser', (req, res) => {
     const { newUsername, newPassword } = req.body;
     const addUserCommand = `rcon.exe -a ${serverConfig.ip}:${serverConfig.port} -p ${serverConfig.rconPassword} "adduser ${newUsername} ${newPassword}"`;
 
     exec(addUserCommand, (error, stdout, stderr) => {
         if (error) {
             console.error(`Erreur lors de l'exécution de la commande adduser : ${error}`);
+            sendOutputToClients(`Erreur lors de l'exécution de la commande adduser : ${error}`);
             return res.status(500).send('Erreur lors de l\'exécution de la commande adduser');
         }
 
         console.log(`Sortie de la commande adduser : ${stdout}`);
+        sendOutputToClients(`Sortie de la commande adduser : ${stdout}`);
         res.send('Commande adduser exécutée avec succès !');
     });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
 });
